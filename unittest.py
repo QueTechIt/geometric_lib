c6d820a82fdc0138e29affbff2703181cef34542
Dmitriy <165498320+QueTechIt@users.noreply.github.com>
2024-12-04 08:37:23 +0300

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -3,148 +3,113 @@ import rectangle
 import circle
 import triangle
 import square
-''' ------ '''
+
 class GeometryTestCase(unittest.TestCase):
-    '''Тестирование функциональности прямоугольника'''
-
-    def test_rectangle_area_with_zero_height(self):
-        # Проверка площади прямоугольника с нулевой высотой
-        self.assertEqual(rectangle.area(8, 0), 0)
-
-    def test_rectangle_area_with_equal_sides(self):
-        # Проверка площади квадрата (прямоугольника с равными сторонами)
-        self.assertEqual(rectangle.area(7, 7), 49)
-
-    def test_rectangle_perimeter_with_zero_height(self):
-        # Проверка периметра прямоугольника с нулевой высотой
-        self.assertEqual(rectangle.perimeter(8, 0), 16)
-
-    def test_rectangle_perimeter_with_valid_dimensions(self):
-        # Проверка периметра прямоугольника с положительными сторонами
-        self.assertEqual(rectangle.perimeter(6, 12), 36)
+    '''Тесты для прямоугольника'''
+    def test_rectangle_area_zero_mul(self):
+        self.assertEqual(rectangle.area(10, 0), 0)
+    
+    def test_rectangle_area_square_mul(self):
+        self.assertEqual(rectangle.area(10, 10), 100)
+    
+    def test_rectangle_perimeter_zero(self):
+        self.assertEqual(rectangle.perimeter(10, 0), 20)
+    
+    def test_rectangle_perimeter(self):
+        self.assertEqual(rectangle.perimeter(5, 10), 30)
 
     '''Негативные тесты для прямоугольника'''
-
-    def test_rectangle_area_with_negative_width(self):
-        # Проверка на ошибку при использовании отрицательной ширины
+    def test_rectangle_area_negative(self):
         with self.assertRaises(ValueError):
-            rectangle.area(-2, 5)
-
-    def test_rectangle_area_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения
+            rectangle.area(-1, 5)
+            
+    def test_rectangle_area_string(self):
         with self.assertRaises(TypeError):
-            rectangle.area("b", 5)
-
-    def test_rectangle_perimeter_with_negative_width(self):
-        # Проверка на ошибку при использовании отрицательной ширины для периметра
+            rectangle.area("a", 5)
+    
+    def test_rectangle_perimeter_negative(self):
         with self.assertRaises(ValueError):
-            rectangle.perimeter(-2, 5)
+            rectangle.perimeter(-1, 5)
 
-    def test_rectangle_perimeter_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для периметра
+
+    def test_rectangle_perimeter_string(self):
         with self.assertRaises(TypeError):
-            rectangle.perimeter("b", 5)
+            rectangle.perimeter("a", 5)
 
-    '''Тестирование функциональности круга'''
-
-    def test_circle_area_with_zero_radius(self):
-        # Проверка площади круга с нулевым радиусом
+    '''Тесты для круга'''
+    def test_circle_area_zero_radius(self):
         self.assertEqual(circle.area(0), 0)
-
-    def test_circle_area_with_positive_radius(self):
-        # Проверка площади круга с положительным радиусом
-        self.assertAlmostEqual(circle.area(4), 50.24, places=2)
-
-    def test_circle_perimeter_with_zero_radius(self):
-        # Проверка периметра круга с нулевым радиусом
+    
+    def test_circle_area(self):
+        self.assertAlmostEqual(circle.area(5), 78.54, places=3)
+    
+    def test_circle_perimeter_zero_radius(self):
         self.assertEqual(circle.perimeter(0), 0)
-
-    def test_circle_perimeter_with_positive_radius(self):
-        # Проверка периметра круга с положительным радиусом
-        self.assertAlmostEqual(circle.perimeter(4), 25.12, places=2)
+    
+    def test_circle_perimeter(self):
+        self.assertAlmostEqual(circle.perimeter(5), 31.416, places=3)
 
     '''Негативные тесты для круга'''
-
-    def test_circle_area_with_negative_radius(self):
-        # Проверка на ошибку при использовании отрицательного радиуса
+    def test_circle_area_negative_radius(self):
         with self.assertRaises(ValueError):
-            circle.area(-3)
+            circle.area(-1)
 
-    def test_circle_perimeter_with_negative_radius(self):
-        # Проверка на ошибку при использовании отрицательного радиуса для периметра
+    def test_circle_perimeter_negative_radius(self):
         with self.assertRaises(ValueError):
-            circle.perimeter(-3)
+            circle.perimeter(-1)
 
-    def test_circle_area_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для площади
+    def test_circle_area_string(self):
         with self.assertRaises(TypeError):
-            circle.area("b")
+            circle.area("a")
 
-    def test_circle_perimeter_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для периметра
+    def test_circle_perimeter_string(self):
         with self.assertRaises(TypeError):
-            circle.perimeter("b")
+            circle.perimeter("a")
 
-    '''Тестирование функциональности треугольника'''
-
-    def test_triangle_area_with_base_and_height(self):
-        # Проверка площади треугольника с заданными основанием и высотой
-        self.assertAlmostEqual(triangle.area(5, 6), 15.0)
-
-    def test_triangle_perimeter_with_valid_sides(self):
-        # Проверка периметра треугольника с корректными длинами сторон
-        self.assertEqual(triangle.perimeter(5, 12, 13), 30)
+    '''Тесты для треугольника'''
+    def test_triangle_area(self):
+        self.assertAlmostEqual(triangle.area(3, 4), 6.0)
+    
+    def test_triangle_perimeter(self):
+        self.assertEqual(triangle.perimeter(3, 4, 5), 12)
 
     '''Негативные тесты для треугольника'''
-
-    def test_triangle_area_with_negative_height(self):
-        # Проверка на ошибку при использовании отрицательной высоты
+    def test_triangle_area_negative(self):
         with self.assertRaises(ValueError):
-            triangle.area(-2, 6)
+            triangle.area(-1, 4)
 
-    def test_triangle_perimeter_with_invalid_sides(self):
-        # Проверка на ошибку при использовании сторон, не образующих треугольник
+    def test_triangle_perimeter_invalid_triangle(self):
         with self.assertRaises(ValueError):
-            triangle.perimeter(5, 12, 20)
+            triangle.perimeter(3, 4, 8)
 
-    def test_triangle_area_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для площади
+    def test_triangle_area_string(self):
         with self.assertRaises(TypeError):
-            triangle.area("b", 6)
+            triangle.area("a", 4)
 
-    def test_triangle_perimeter_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для периметра
+    def test_triangle_perimeter_string(self):
         with self.assertRaises(TypeError):
-            triangle.perimeter(5, 12, "d")
+            triangle.perimeter(3, 4, "c")
 
-    '''Тестирование функциональности квадрата'''
-
-    def test_square_area_with_side_length(self):
-        # Проверка площади квадрата с заданной длиной стороны
-        self.assertEqual(square.area(6), 36)
-
-    def test_square_perimeter_with_side_length(self):
-        # Проверка периметра квадрата с заданной длиной стороны
-        self.assertEqual(square.perimeter(5), 20)
+    '''Тесты для квадрата'''
+    def test_square_area(self):
+        self.assertEqual(square.area(5), 25)
+    
+    def test_square_perimeter(self):
+        self.assertEqual(square.perimeter(4), 16)
 
     '''Негативные тесты для квадрата'''
-
-    def test_square_area_with_negative_side(self):
-        # Проверка на ошибку при использовании отрицательной длины стороны
+    def test_square_area_negative(self):
         with self.assertRaises(ValueError):
-            square.area(-2)
+            square.area(-1)
 
-    def test_square_perimeter_with_negative_side(self):
-        # Проверка на ошибку при использовании отрицательной длины стороны для периметра
+    def test_square_perimeter_negative(self):
         with self.assertRaises(ValueError):
-            square.perimeter(-10)
+            square.perimeter(-1)
 
-    def test_square_area_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для площади
+    def test_square_area_string(self):
         with self.assertRaises(TypeError):
             square.area("a")
 
-    def test_square_perimeter_with_string_input(self):
-        # Проверка на ошибку при передаче строкового значения для периметра
+    def test_square_perimeter_string(self):
         with self.assertRaises(TypeError):
-            square.perimeter("b")
+            square.perimeter("a")
